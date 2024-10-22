c1ebbeaf6ffc91eca76ccea144802de6c9c371c2
zobirofkir <zobirofkir19@gmail.com>
2024-10-22 21:06:51 +0100

    download filament

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('image')->nullable();
+            $table->string('role')->default('guest');
             $table->rememberToken();
             $table->timestamps();
         });
