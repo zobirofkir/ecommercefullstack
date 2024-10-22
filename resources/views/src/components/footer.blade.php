35eda7fe9f7f6ec8ef7053dfd3b294db980764b8
zobirofkir <zobirofkir19@gmail.com>
2024-10-22 21:44:37 +0100

    create footer component

--- resources/views/src/components/footer.blade.php
+++ resources/views/src/components/footer.blade.php
@@ -1,1 +1,22 @@
-<h1>footer</h1>
+<footer class="bg-green-100 text-black py-8 mt-10">
+    <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div class="text-center md:text-left">
+            <p class="text-black mt-2">© 2024 Woddy. All Rights Reserved.</p>
+        </div>
+
+        <div class="flex gap-4 mt-4 md:mt-0">
+            <a href="#" class="text-gray-400 hover:text-black">
+                <i class="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-black">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-black">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-black">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+        </div>
+    </div>
+</footer>
