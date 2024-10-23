073eef75437ea556900711d9d73858600e31fb9f
zobirofkir <zobirofkir19@gmail.com>
2024-10-23 11:33:25 +0100

    add auto generated image from slider

--- resources/views/src/components/slider.blade.php
+++ resources/views/src/components/slider.blade.php
@@ -1,11 +1,9 @@
 <div class="relative container mx-auto bg-white shadow-lg rounded-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out mb-10 overflow-hidden">
     <div id="slider" class="flex transition-transform duration-700 ease-in-out w-full">
 
-        <!-- Sliders -->
-
         @foreach ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] as $item)
             <div class="flex flex-col md:flex-row justify-between items-center md:gap-4 gap-8 w-full flex-none">
-                <!-- Text Section -->
+
                 <div class="md:w-1/2 w-full p-4 md:p-8">
                     <h1 class="text-black md:text-left text-center font-bold text-3xl md:text-4xl">
                         Woody Product {{ $item }}
@@ -15,10 +13,9 @@
                     </p>
                 </div>
 
-                <!-- Image Section -->
                 <div class="w-full md:w-1/2 p-4 md:p-8 flex justify-center items-center">
                     <img 
-                        src="https://images.samsung.com/is/image/samsung/p6pim/n_africa/2302/gallery/n-africa-galaxy-s23-s911-sm-s911blicmwd-534844949?$650_519_PNG$"
+                        src="https://picsum.photos/200/150"
                         alt="Product image"
                         class="w-full h-full md:max-h-full object-cover rounded-lg shadow-lg hover:rotate-6 transition-transform duration-300"
                     >
