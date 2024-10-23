fc85784b663835b3c8617ac7e885139117481dc4
zobirofkir <zobirofkir19@gmail.com>
2024-10-23 01:26:13 +0100

    add offer component

---
+++ resources/views/src/components/offer.blade.php
@@ -0,0 +1,8 @@
+<div class="container mx-auto">
+    <div class="flex flex-col md:flex-row justify-between items-center">
+        <div class="flex justify-between items-center w-full bg-purple-950 p-20 rounded-md hover:rotate-3 transition-transform duration-300">
+            <h3 class="text-white font-bold text-3xl hover:-rotate-3 transition-transform duration-300">Get 10% off on your first order</h3>
+            <p class="text-white text-lg font-semibold hover:-rotate-3 transition-transform duration-300">Sign up for our newsletter to get 10% off your first order</p>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout>
     <div>
+        @include('src.components.offer')
+
         @include('src.components.products')
     </div>
 </x-app-layout>
