3db60dc238557eb6bea9db83f4fe608f053aacd4
zobirofkir <zobirofkir19@gmail.com>
2024-10-23 19:26:40 +0100

    update offer and subscription

--- resources/views/src/components/offer.blade.php
+++ resources/views/src/components/offer.blade.php
@@ -1,11 +1,13 @@
-<div class="container mx-auto shad">
-    <div class="flex flex-col md:flex-row justify-between items-center ">
-        <div class="flex flex-col gap-10 justify-between items-center w-full bg-purple-950 p-20 rounded-md hover:rotate-3 transition-transform duration-300">
-            <h3 class="text-white font-bold text-3xl hover:-rotate-3 transition-transform duration-300">Get 10% off on your first order</h3>
-            <p class="text-white text-lg font-semibold hover:-rotate-3 transition-transform duration-300">Sign up for our newsletter to get 10% off your first order</p>
-            <div class="flex gap-4">
-                <a href="{{url('/register')}}" class="text-black font-bold hover:-rotate-3 transition-transform duration-300 bg-white px-8 py-3 rounded-full">Sign up</a>
+@if (!Auth::check())
+    <div class="container mx-auto shad">
+        <div class="flex flex-col md:flex-row justify-between items-center ">
+            <div class="flex flex-col gap-10 justify-between items-center w-full bg-purple-950 p-20 rounded-md hover:rotate-3 transition-transform duration-300">
+                <h3 class="text-white font-bold text-3xl hover:-rotate-3 transition-transform duration-300">Get 10% off on your first order</h3>
+                <p class="text-white text-lg font-semibold hover:-rotate-3 transition-transform duration-300">Sign up for our newsletter to get 10% off your first order</p>
+                <div class="flex gap-4">
+                    <a href="{{url('/register')}}" class="text-black font-bold hover:-rotate-3 transition-transform duration-300 bg-white px-8 py-3 rounded-full">Sign up</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
