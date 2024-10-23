8b505723d89bb9501dad1b0fb2491e963779d9cc
zobirofkir <zobirofkir19@gmail.com>
2024-10-23 11:34:47 +0100

    add rotation from blogs component

--- resources/views/src/components/blogs/blogs.blade.php
+++ resources/views/src/components/blogs/blogs.blade.php
@@ -9,14 +9,14 @@
                 <div class="bg-white shadow-lg rounded-lg p-4 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:-rotate-3">
                     <a href="#" class="">
                         <div class="flex justify-center items-center">
-                            <img src="https://picsum.photos/200/300" alt="Blog Image" class="w-full md:h-[400px] h-[200px] object-cover object-center rounded">
+                            <img src="https://picsum.photos/200/300" alt="Blog Image" class="w-full md:h-[400px] h-[200px] object-cover object-center rounded hover:rotate-3 transition-transform duration-300">
                         </div>
                         <div class="flex flex-col">
-                            <h1 class="text-xl font-bold text-center mt-4 text-black hover:-rotate-3 transition-transform duration-300">Blog Title</h1>
-                            <p class="text-black hover:-rotate-3 font-semibold py-2 transition-transform duration-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h1 class="text-xl font-bold text-center mt-4 text-black hover:rotate-3 transition-transform duration-300">Blog Title</h1>
+                            <p class="text-black hover:rotate-3 font-semibold py-2 transition-transform duration-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
                         <div class="flex justify-start items-center">
-                            <a href="#" class="text-green-500 hover:text-green-600 font-semibold hover:-rotate-3 transition-transform duration-300">Read More</a>
+                            <a href="#" class="text-green-500 hover:text-green-600 font-semibold hover:rotate-3 transition-transform duration-300">Read More</a>
                         </div>
                     </a>
                 </div>
