6122b25276a38c7d302f8012ce3da573b824809e
zobirofkir <zobirofkir19@gmail.com>
2024-10-25 00:00:25 +0100

    add title from category-show

--- resources/views/src/components/categories/category-show.blade.php
+++ resources/views/src/components/categories/category-show.blade.php
@@ -1,4 +1,32 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-yellow-sofa-furniture-png-image_11548333.png">
+
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="src/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
+
+
+    <title>
+        @if (Auth::check())
+            {{ Auth::user()->name }}
+        @else
+            {{ config('app.name') }}
+        @endif
+    </title>
+</head>
+<body>
+    @include('src.components.header')
+    
+    @include('src.components.search')
+
+    @include('src.components.categories')
+
 
     @php
         $results = App\Services\Facades\CategoryFacade::get();
@@ -18,10 +46,15 @@
         }
     @endphp
 
-    <div class="container mx-auto mt-10 mb-10 p-6">
+    <div class="container mx-auto -mt-10 mb-10 p-6">
         <div class="flex justify-center">
-            <h2 class="text-2xl font-bold mb-4">Products in {{ $currentCategory->title }}:</h2>
+            <h2 class="text-2xl font-bold mb-4">Products in {{ $currentCategory->title }}</h2>
         </div>
+
+        <div class="mt-6 mb-20">
+            @include('src.components.slider')
+        </div>
+
         <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
         @foreach($categoryItem as $product)
             <div class="bg-white shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
@@ -43,4 +76,10 @@
         </div>
     </div>
 
-</x-app-layout>
+    @include('src.components.lister')
+    @include('src.components.footer')
+
+    <script src="{{asset('src/js/dropdown.js')}}"></script>
+    <script src="{{asset('src/js/slider.js')}}"></script>
+</body>
+</html>
