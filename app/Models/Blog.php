ad7011bc06596370ef3424eabdc47b71e8d9b884
zobirofkir <zobirofkir19@gmail.com>
2024-10-24 08:17:27 +0100

    create blog resource dashboard

---
+++ app/Models/Blog.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Blog extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'slug'
+    ];
+
+    protected $casts = [
+        'image' => 'array',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($blog) {
+            $blog->slug = Str::slug($blog->title);
+        });
+
+        static::updating(function ($blog) {
+            if ($blog->isDirty('title')) {
+                $blog->slug = Str::slug($blog->title);
+            }
+        });
+    }
+}
