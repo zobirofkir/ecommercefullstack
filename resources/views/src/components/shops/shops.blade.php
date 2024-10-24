498485a678d630ec7f4705d64a1d866bc99cfb1c
zobirofkir <zobirofkir19@gmail.com>
2024-10-24 11:52:49 +0100

    change products

--- resources/views/src/components/shops/shops.blade.php
+++ resources/views/src/components/shops/shops.blade.php
@@ -1,23 +1,23 @@
 <div class="container mx-auto p-6 mb-10">
-    <h1 class="text-3xl font-bold mb-6 text-center">Our Products</h1>
-    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <h1 class="text-3xl font-bold mb-6 text-center text-gray-900">Our Products</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         @php
             $items = App\Services\Facades\ProductFacade::index();
             $products = $items['products'];
         @endphp
         @foreach ($products as $product)
-            <div class="bg-white shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105">
+            <div class="bg-white shadow-lg rounded-lg overflow-hidden transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl">
                 <img src="{{ asset('storage/' . $product->images[0]) }}" class="w-full h-48 object-cover object-center" alt="Product Image">
                 <div class="p-4">
-                    <h3 class="text-xl font-semibold text-gray-800 hover:text-green-500 transition duration-200">
+                    <h3 class="text-lg font-semibold text-gray-800 hover:text-green-600 transition duration-200">
                         {{ $product->title }}    
                     </h3>
                     <p class="text-gray-600 mt-2 truncate">
                         {{ $product->description }}
                     </p>
                     <div class="flex justify-between items-center mt-4">
-                        <a href="{{ route('shops.show', $product->slug) }}" class="text-green-500 md:text-start text-center hover:text-green-600 font-semibold transition duration-200">View Details</a>
-                        <a href="#" class="text-green-500 md:text-start text-center hover:text-green-600 font-semibold transition duration-200">Add to Cart</a>
+                        <a href="{{ route('shops.show', $product->slug) }}" class="bg-green-500 text-white px-4 py-2 rounded-lg text-sm font-semibold transition duration-200 hover:bg-green-600">View Details</a>
+                        <a href="#" class="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg text-sm font-semibold transition duration-200 hover:bg-gray-300">Add to Cart</a>
                     </div>
                 </div>
             </div>
