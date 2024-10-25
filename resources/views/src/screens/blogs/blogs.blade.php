7b3e08f8eea72c72de70c162e3fc8ea1946abf79
zobirofkir <zobirofkir19@gmail.com>
2024-10-25 03:38:24 +0100

    keep slider only from home page

--- resources/views/src/screens/blogs/blogs.blade.php
+++ resources/views/src/screens/blogs/blogs.blade.php
@@ -1,5 +1,33 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="https://png.pngtree.com/png-vector/20240125/ourmid/pngtree-yellow-sofa-furniture-png-image_11548333.png">
 
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="src/css/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
+
+
+    <title>
+        @if (Auth::check())
+            {{ Auth::user()->name }}
+        @else
+            {{ config('app.name') }}
+        @endif
+    </title>
+</head>
+<body>
+    @include('src.components.header')
+    
+    @include('src.components.search')
+
+    @include('src.components.categories')
+
+    
     @foreach ($blogs->take(1) as $blog)
 
     <div class="container mx-auto bg-gray-100 p-10 rounded-md md:block hidden">
@@ -29,4 +57,12 @@
 
     @endforeach
     @include('src.components.blogs.blogs')
-</x-app-layout>
+    
+    @include('src.components.lister')
+    @include('src.components.footer')
+
+    <script src="{{asset('src/js/dropdown.js')}}"></script>
+    <script src="{{asset('src/js/slider.js')}}"></script>
+
+</body>
+</html>
